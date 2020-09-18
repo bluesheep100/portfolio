3ca8c32764d51cb2f64cb4c883aeba0089160207
Anders Aagaard Schütt <bluesheep100@hotmail.com>
2020-09-18 08:57:52 +0200

    Replace placeholder text in portfolio project page

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -7,14 +7,13 @@
                 <div class="card">
                     <h5 class="card-title">My Portfolio</h5>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt eos explicabo facere libero
-                        nam quisquam ut vitae. Aliquid debitis eveniet, ipsa itaque iusto maiores natus omnis quaerat
-                        quidem repudiandae, voluptatum.
+                        This is the website you're currently on! I decided on making this when I discovered <a href="https://www.gethalfmoon.com/">Halfmoon</a>,
+                        because I was so impressed by its simplicity and ease of use, that I simply <strong>had</strong> to try it out.<br>
+                        Halfmoon also includes some <i>wonderful</i> features right out of the box, such as native dark mode support. (just try pressing Shift+d)
                     </p>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A distinctio dolores earum eveniet ex
-                        labore laudantium minima minus molestias, mollitia, officia quaerat rem saepe temporibus veniam!
-                        Amet aspernatur harum recusandae.
+                        As mentioned, the frontend uses Halfmoon for styling, and the backend is running <a href="https://laravel.com/">Laravel</a> 8.2, which is only natural,
+                        because Laravel is my favorite framework. I love it for its logical structure, easy-to-use boilerplates and CLI tools.
                     </p>
                 </div>
             </div>
