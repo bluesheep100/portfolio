306fd928f49c0b7d0729541c27148802a3c123c5
Anders Aagaard Schütt <bluesheep100@hotmail.com>
2021-02-17 15:31:18 +0100

    Add a delete button and the ability to specify player counter

--- resources/views/yahtzee.blade.php
+++ resources/views/yahtzee.blade.php
@@ -7,12 +7,28 @@
                 <div class="card">
                     @unless (session()->has('yahtzee'))
                         <form action="{{ route('yahtzee_create') }}" method="POST">
+                            @csrf
+                            <p>Select the amount of players, and click here to create a new sheet.</p>
+                            <div class="form-group">
+                                <input type="number"
+                                       name="player_count"
+                                       class="form-control w-50 @error('player_count') is-invalid @enderror"
+                                       min="1"
+                                       max="6"
+                                       value="1"
+                                >
+                            </div>
                             <button type="submit" class="btn btn-primary">
-                                Start a game
+                                Start
                             </button>
                         </form>
                     @else
-                        <!-- Add a Yahtzee board here -->
+                        <form action="{{ route('yahtzee_delete') }}" method="post">
+                            <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?');">
+                                Delete sheet
+                            </button>
+                        </form>
+
                     @endif
                 </div>
             </div>
