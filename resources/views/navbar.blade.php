6c5e0f1a01ee61f6e8c363cfa90f08ba805179a7
Anders Aagaard Schütt <AASc.skp@plato.local>
2022-08-11 11:56:26 +0200

    Update CSS

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -5,7 +5,8 @@
             Anders Schütt
         </div>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-collapse"
+                aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbar-collapse">
